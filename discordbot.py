398f1c0d237395976b85f5f474b7a0e9cc2429db
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-11 14:52:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,12 +2,9 @@ from discord.ext import commands
 import os
 import traceback
 
-
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-    
 
 @bot.event
 async def on_command_error(ctx, error):
