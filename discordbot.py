fa224206f7ad3a6e85d28f572f4176dc0ee7c5b8
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-11 15:40:19 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,5 +4,9 @@ import traceback
 
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@client.event
+async def on_massege(messege):
+  if messege.content=="/neko":
+    await messege.channel.send("ねこ
 
 client.run(token)
