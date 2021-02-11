bc1f81fd721a7e62c22370b1a810787d8ac5def4
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-11 14:53:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,23 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    # 発言したチャンネルのカテゴリ内にチャンネルを作成する非同期関数
+async def create_channel(message, channel_name):
+    category_id = message.channel.category_id
+    category = message.guild.get_channel(category_id)
+    new_channel = await category.create_text_channel(name=channel_name)
+    return new_channel
+
+# 発言時に実行されるイベントハンドラを定義
+@client.event
+async def on_message(message):
+    if message.content.startswith('/mkch'):
+        # チャンネルを作成する非同期関数を実行して Channel オブジェクトを取得
+        new_channel = await create_channel(message, channel_name='new')
+
+        # チャンネルのリンクと作成メッセージを送信
+        text = f'{new_channel.mention} を作成しました'
+        await message.channel.send(text)
 
 
 bot.run(token)
