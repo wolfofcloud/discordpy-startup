6dabf95a21d1b4e4473e34d30e33977647e47c99
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-11 14:47:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,21 +19,6 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-   
-@bot.command()
-async def create_channel(message, channel_name):
-    category_id = message.channel.category_id
-    category = message.guild.get_channel(category_id)
-    new_channel = await category.create_text_channel(name=channel_name)
-    return new_channel
-
-@client.event
-async def on_message(message):
-    if message.content.startswith('/create'):
-        new_channel = await create_channel(message, channel_name='new')
-        text = f'{new_channel.mention} を作成しました'
-        await message.channel.send(text)
-       
 
 
 bot.run(token)
