02c9685f8658aedc3f47597009ad20212336a700
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-11 16:32:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,6 +6,7 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
+
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
@@ -15,5 +16,11 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def test(ctx,arg):
     await ctx.send(arg)
+    
+@bot.command()
+async def create(ctx,arg):
+    category = '708239634051760179'
+    new_channel = await categoty.create_text_channl(name=arg)
+    return new_channel
     
 bot.run(token)
