4f7382182c963e2b2fad6e4000c33b9a451c23d2
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-11 15:37:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,36 +3,6 @@ import discord
 import traceback
 
 token = os.environ['DISCORD_BOT_TOKEN']
-client=discord.Client()
 
-
-@client.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@client.event
-async def ping(ctx):
-    await ctx.send('pong')
-
-# 発言したチャンネルのカテゴリ内にチャンネルを作成する非同期関数
-async def create_channel(message, channel_name):
-    category_id = message.channel.category_id
-    category = message.guild.get_channel(category_id)
-    new_channel = await category.create_text_channel(name=channel_name)
-    return new_channel
-
-# 発言時に実行されるイベントハンドラを定義
-@client.event
-async def on_message(message):
-    if message.content.startswith('/mkch'):
-        # チャンネルを作成する非同期関数を実行して Channel オブジェクトを取得
-        new_channel = await create_channel(message, channel_name='new')
-
-        # チャンネルのリンクと作成メッセージを送信
-        text = f'{new_channel.mention} を作成しました'
-        await message.channel.send(text)
 
 client.run(token)
