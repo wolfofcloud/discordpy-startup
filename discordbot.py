fda548ee602d46b996b374f59ccf36cfaf11a497
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-11 16:21:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,7 +11,5 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-
-
-
+    
 bot.run(token)
