1c26ee8ac5fcbe80c652d07cd768cec377246c98
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-12 17:04:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,5 +14,19 @@ async def on_message(message):
         return
     if message.content == '/neko':
         await message.channel.send('にゃーん')
+        
+
+async def create_channel(message, channel_name):
+    category_id = message.channel.category_id
+    category = message.guild.get_channel(category_id)
+    new_channel = await category.create_text_channel(name=channel_name)
+    return new_channel
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('/mkch'):
+        new_channel = await create_channel(message, channel_name='new')
+        text = f'{new_channel.mention} を作成しました'
+        await message.channel.send(text)
 
 client.run(TOKEN)
