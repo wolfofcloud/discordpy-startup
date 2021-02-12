d72a7a6cad04aa6d40480f463d4d446ce772890d
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-12 17:31:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,17 +15,21 @@ async def on_message(message):
     if message.content == '/neko':
         await message.channel.send('にゃーん')
 
+# 発言したチャンネルのカテゴリ内にチャンネルを作成する非同期関数
 async def create_channel(message, channel_name):
     category_id = message.channel.category_id
     category = message.guild.get_channel(category_id)
     new_channel = await category.create_text_channel(name=channel_name)
     return new_channel
 
+# 発言時に実行されるイベントハンドラを定義
 @client.event
 async def on_message(message):
     if message.content.startswith('/mkch'):
+        # チャンネルを作成する非同期関数を実行して Channel オブジェクトを取得
         new_channel = await create_channel(message, channel_name='new')
 
+        # チャンネルのリンクと作成メッセージを送信
         text = f'{new_channel.mention} を作成しました'
         await message.channel.send(text)
 
