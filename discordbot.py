65db43b4db690947878e9da754278d7fd6e3205b
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-12 16:59:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,10 +12,6 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
-    
-@bot.command()
-async def test(ctx,arg):
-    await ctx.send(arg)
 
 @bot.event
 async def create_channel(channel_neme):
@@ -23,6 +19,9 @@ async def create_channel(channel_neme):
     new_channel = await category.create_text_channel(name=channel_name)
     return new_channel
 
+@bot.command()
+async def test(ctx,arg):
+    await ctx.send(arg)
 
 @bot.command()
 async def create(ctx,arg):
