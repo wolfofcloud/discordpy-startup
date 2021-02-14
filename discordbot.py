e715c561c77888263554173d5c7e12ee91bfc015
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-14 17:49:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,6 +4,13 @@ import os
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
 
+# 発言したチャンネルのカテゴリ内にチャンネルを作成する非同期関数
+async def create_channel(message, channel_name):
+    category_id = message.channel.category_id
+    category = message.guild.get_channel(category_id)
+    new_channel = await category.create_text_channel(name=channel_name)
+    return new_channel
+
 @client.event
 async def on_ready():
     print('ログインしました')
@@ -15,12 +22,6 @@ async def on_message(message):
     if message.content == '/neko':
         await message.channel.send('にゃーん')
 
-# 発言したチャンネルのカテゴリ内にチャンネルを作成する非同期関数
-async def create_channel(message, channel_name):
-    category_id = message.channel.category_id
-    category = message.guild.get_channel(category_id)
-    new_channel = await category.create_text_channel(name=channel_name)
-    return new_channel
 
 # 発言時に実行されるイベントハンドラを定義
 @client.event
