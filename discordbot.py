c347c3cf14f9a2c700f6dbde774115648bf425a5
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-17 17:57:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,10 +4,11 @@ import os
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
 # get_guild
-guild2 = messege.guild.get_guild(661027381980561409)
+
 
 # 発言したチャンネルのカテゴリ内にチャンネルを作成する非同期関数
 async def create_channel(message, channel_name,overwrites):
+    guild2 = messege.guild.get_guild(661027381980561409)
     category_id = 687069139067600897
     category = message.guild.get_channel(category_id)
     new_channel = await guild2.category.create_text_channel(name=channel_name,overwrites=overwrites)
@@ -25,6 +26,7 @@ async def on_message(message):
         cot=cot.replace('/create ','')
         guild1 = message.guild
         new_role = await guild1.create_role(name=cot)
+        guild2 = messege.guild.get_guild(661027381980561409)
         overwrites = {
             guild2.default_role: discord.PermissionOverwrite(read_messages=False),
             guild2.me: discord.PermissionOverwrite(read_messages=True)
