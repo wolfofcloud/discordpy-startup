04007fe5f8f7027cd9394b0a2df5907d1df7439c
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-18 15:46:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,7 +9,7 @@ client = discord.Client()
 
 async def create_channel(message, channel_name,overwrites):
     guild1 = message.guild
-    guild2 = messege.get_guild(661027381980561409)
+    guild2 = client.get_guild(661027381980561409)
     category_id = 687069139067600897
     category = message.guild1.get_channel(category_id)
     new_channel = await guild2.create_text_channel(name=channel_name,overwrites=overwrites,category=category)
@@ -27,7 +27,7 @@ async def on_message(message):
         cot=cot.replace('/create ','')
         guild1 = message.guild
         new_role = await guild1.create_role(name=cot)
-        guild2 = messege.get_guild(661027381980561409)
+        guild2 = client.get_guild(661027381980561409)
         overwrites = {
             guild2.default_role: discord.PermissionOverwrite(read_messages=False),
             guild2.me: discord.PermissionOverwrite(read_messages=True)
