8f657df98aa6781473d832e1275c084c97fc4c0c
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-23 23:54:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,41 +4,18 @@ import traceback
 
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
-# get_guild
 
-
-
-async def create_channel(message, channel_name):
-    guild2 = client.get_guild(661027381980561409)
-    category_id = 687069139067600897
-    category = message.guild1.get_channel(category_id)
-    new_channel = await create_text_channel(name=channel_name,category=category)
-    return new_channel
-
-@client.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-@client.event
-async def on_ready():
-    print('ログインしました')
-
-        
 @client.event
 async def on_message(message):
     if message.content.startswith('/create'):
-        cot=message.content
-        cot=cot.replace('/create ','')
-        guild1 = message.guild
-        new_role = await guild1.create_role(name=cot)
-        
-        # チャンネルを作成する非同期関数を実行して Channel オブジェクトを取得
-        new_channel = await create_channel(message, channel_name=cot)
-        # チャンネルのリンクと作成メッセージを送信
-        text = f'{new_channel.mention} を作成しました'
-        await message.channel.send(text)
+        # discord.Guild.create_text_channelを使用する
+        guild = client.get_guild(661027381980561409)
+        # @everyoneは発言できないが、自分はできる
+        overwrites = {guild.default_role: discord.PermissionOverWrite(send_messages=False),
+                      guild.me: discord.PermissionOverWrite(send_messages=True)
+                     }
+        new_channel = await guild.create_text_channel(name="お知らせ", overwrites=overwrites, topic="お知らせを表示します。")
+        print(f"#{new_channel.name} を作成しました。")
         
 
 client.run(TOKEN)
