e960a81d6d507e97b06631e8687c7b531e8bfbb3
wolfofcloud <78695871+wolfofcloud@users.noreply.github.com>
2021-02-27 17:24:21 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,18 +4,41 @@ import traceback
 
 TOKEN = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
+# get_guild
 
+
+
+async def create_channel(message, hannel_name,):
+    guild1 = message.guild
+    category_id = 687069139067600897
+    category = message.guild1.get_channel(category_id)
+    new_channel = await create_text_channel(name=channel_name,category=category)
+    return new_channel
+
+@client.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+
+@client.event
+async def on_ready():
+    print('ログインしました')
+
+        
 @client.event
 async def on_message(message):
     if message.content.startswith('/create'):
-        guild = client.get_guild(661027381980561409)
         cot=message.content
         cot=cot.replace('/create ','')
-        overwrites = {guild.default_role: discord.PermissionOverWrite(send_messages=False),
-                      guild.me: discord.PermissionOverWrite(send_messages=True)
-                     }
-        new_channel = await guild.create_text_channel(name=cot, overwrites=overwrites)
-        print(f"#{new_channel.name} を作成しました。")
+        guild1 = message.guild
+        new_role = await guild1.create_role(name=cot)
+        guild2 = client.get_guild(661027381980561409)
+        # チャンネルを作成する非同期関数を実行して Channel オブジェクトを取得
+        new_channel = await create_channel(message, channel_name=cot,)
+        # チャンネルのリンクと作成メッセージを送信
+        text = f'{new_channel.mention} を作成しました'
+        await message.channel.send(text)
         
 
 client.run(TOKEN)
